1ef9a12f69e1909e9eb69b93512a1932b1aa9aee
Anubhav Gupta <37771611+meetanubhav@users.noreply.github.com>
2020-03-05 17:42:57 +0530

    Initailes with view , model , controller and prev deleted repo configs

---
+++ ResumeBuilder/ResumeBuilder/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ResumeBuilder.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+
+        public string UserName { get; set; }
+
+        [DataType(DataType.Password)]
+        [RegularExpression("[a-zA-Z]{3,14}$", ErrorMessage = "Invalid password format")]
+        public string UserPassword { get; set; }
+    }
+}
