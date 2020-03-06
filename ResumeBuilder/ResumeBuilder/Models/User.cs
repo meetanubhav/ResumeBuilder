1b2d99ae780b47cfb15ebbe29e75b6c973022c21
Anubhav Gupta <37771611+meetanubhav@users.noreply.github.com>
2020-03-06 12:45:55 +0530

    change in controller

--- ResumeBuilder/ResumeBuilder/Models/User.cs
+++ ResumeBuilder/ResumeBuilder/Models/User.cs
@@ -10,11 +10,11 @@ namespace ResumeBuilder.Models
     {
         public int UserID { get; set; }
 
-        [Required(ErrorMessage = "Enter User name")]
+        [Required]
         public string Username { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        //[StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
