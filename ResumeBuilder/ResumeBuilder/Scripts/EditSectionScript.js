d6f90c451762d3796ebfac74582449b1efc627ff
bhabani-maharana <59605993+bhabani-maharana@users.noreply.github.com>
2020-03-06 17:24:46 +0530

    Edit Changes to Work Experience

--- ResumeBuilder/ResumeBuilder/Scripts/EditSectionScript.js
+++ ResumeBuilder/ResumeBuilder/Scripts/EditSectionScript.js
@@ -1,5 +1,5 @@
 ﻿$(document).ready(function () {
-    $('.js-show-summary .js-show-education').hide();
+    $('.js-show-summary .js-show-education .js-show-language .js-show-skills').hide();
 
     $('.summary').on("click", function (e) {
         e.preventDefault();
@@ -9,5 +9,24 @@
     $('.education').on("click", function (e) {
         e.preventDefault();
         $('.js-show-education').toggle(300);
+    });
+
+    $('input[name = "optradio"]').on("click", function () {
+        if ($(this).attr('value') == "10th" || $(this).attr('value') == "12th") {
+            $('input[name = "stream"]').hide();
+            $('input[name = "university"]').hide();
+        }
+        else {
+            $('input[name = "stream"]').show();
+            $('input[name = "university"]').show();
+        }
+    });
+    $('input[name = "optradio"]').on("click", function () {
+        if ($(this).attr('value') == "percentage") {
+            $('input[name = "gradetype"]').attr('placeholder', "Percentage");
+        }
+        else {
+            $('input[name = "gradetype"]').attr('placeholder', "CGPA");
+        }
     });
 });
