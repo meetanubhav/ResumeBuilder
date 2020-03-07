7056950edefe7f1a56936cd40fe6e0e015d37999
Anubhav Gupta <37771611+meetanubhav@users.noreply.github.com>
2020-03-08 01:11:16 +0530

    session validated

--- ResumeBuilder/ResumeBuilder/Controllers/ResumeController.cs
+++ ResumeBuilder/ResumeBuilder/Controllers/ResumeController.cs
@@ -21,10 +21,10 @@ namespace ResumeBuilder.Controllers
             if (ModelState.IsValid)
             {
                 var getUserId = db.Users.Where(x => x.Username == user.Username);
-                var userdata = db.Users.SingleOrDefault(x => x.Username == user.Username);
+                var userData = db.Users.SingleOrDefault(x => x.Username == user.Username);
                 if (getUserId.Where(x => x.Password == user.Password).Any())
                 {
-                    Session["userid"] = userdata.UserID;
+                    Session["userId"] = userData.UserID;
                     return RedirectToAction("Dashboard");
                 }
                 else
@@ -46,19 +46,26 @@ namespace ResumeBuilder.Controllers
 
         //[Authorize]
         public ActionResult Dashboard()
-        {
+        { 
             return View();
         }
 
         //[Authorize]
-        public ActionResult Edit()
+        public ActionResult Edit(int? userId)
         {
-            return View();
+            if (userId != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Dashboard");
+            }
         }
-
-        public void SendData()
+        public ActionResult SignOut()
         {
-            
+            Session.Abandon();
+            return RedirectToAction("Login");
         }
     }
 }
