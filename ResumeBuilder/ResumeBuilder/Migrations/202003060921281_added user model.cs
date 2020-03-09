4054bf68e041b3925e87aae6bcd3e2589dee9ad5
Devesh Kaushik <30469038+deveshkau@users.noreply.github.com>
2020-03-09 13:05:13 -0700

    Revert "Changed migration"
    This reverts commit aa74707fa8b6c9dae1de27ed3f4aa79ce6f2802d.

---
+++ ResumeBuilder/ResumeBuilder/Migrations/202003060921281_added user model.cs
@@ -0,0 +1,20 @@
+﻿namespace ResumeBuilder.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedusermodel : DbMigration
+    {
+        public override void Up()
+        {
+     
+            AlterColumn("dbo.Users", "Username", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Users", "Username", c => c.String());
+            DropColumn("dbo.Users", "Password");
+        }
+    }
+}
