1ef91d1e2fb0ab94a0e23029755b8eb5e2bc36d2
Devesh Kaushik <30469038+deveshkau@users.noreply.github.com>
2020-03-09 02:21:08 -0700

    added interface and controller

---
+++ ResumeBuilder/ResumeBuilder/Repository/IResumeBuilderRepository.cs
@@ -0,0 +1,26 @@
+﻿using ResumeBuilder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResumeBuilder.Repository
+{
+    public interface IResumeBuilderRepository
+    {
+        bool AddBasicInformation(UserInfo userInfo);
+        string AddOrUpdateEducation(Education education, int idUser);
+        int GetIdPerson(string email);
+        string AddOrUpdateExperience(WorkExperience workExperience, int idUser);
+        bool AddSkill(Skill skill, int idUser);
+        bool AddProject(Project project, int idUser);
+        bool AddLanguage(Language language, int idUser);
+        User GetBasicInfo(int idUser);
+        IQueryable<Education> GetEducationById(int idUser);
+        IQueryable<WorkExperience> GetWorkExperienceById(int idUser);
+        IQueryable<Skill> GetSkillsById(int idUser);
+        IQueryable<Project> GetProjectById(int idUser);
+        IQueryable<Language> GetLanguageById(int idUser);
+    }
+}
