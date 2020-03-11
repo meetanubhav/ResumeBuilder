f2b62601a4a9cfc7df2abcc1a218cc56208821b5
rishabdaga <41539806+rishabdaga@users.noreply.github.com>
2020-03-11 14:38:19 +0530

    cache changes

--- ResumeBuilder/ResumeBuilder/Controllers/ResumeController.cs
+++ ResumeBuilder/ResumeBuilder/Controllers/ResumeController.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 using System.Security.Claims;
 using System.Security.Authentication;
 =======
 using System.Web.Security;
 using System.Data.Entity;
+>>>>>>> Stashed changes
+=======
+using System.Web.Security;
 >>>>>>> Stashed changes
 
 using System.Data.Entity;
@@ -42,6 +46,7 @@ namespace ResumeBuilder.Controllers
                 switch (userId.Value)
                 {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
                     case -1:
                         message = "Username and/or password is incorrect.";
                         break;
@@ -62,19 +67,48 @@ namespace ResumeBuilder.Controllers
                 }
 
                 ViewBag.Message = message;
+=======
+                    FormsAuthentication.SetAuthCookie(userData.UserID.ToString(), false);
+                    return RedirectToAction("Dashboard", "Resume");
+                }
+                else
+                {
+                    ModelState.AddModelError("","Invalid UserName or Password");
+                }
+>>>>>>> Stashed changes
                 return View(user);
             }
         }
 
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         [Authorize]
 =======
         [Authorize(Roles = "Users")]
 >>>>>>> Stashed changes
+=======
+        [Authorize]
+>>>>>>> Stashed changes
         public ActionResult Dashboard()
         {
+<<<<<<< Updated upstream
 >>>>>>> Stashed changes
             return View();
+=======
+            return Content("..");
+        }
+        [Authorize]
+        public ActionResult Edit(int? userId)
+        {
+            if (userId != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Dashboard");
+            }
+>>>>>>> Stashed changes
         }
 <<<<<<< Updated upstream
         public ActionResult Dashboard()
