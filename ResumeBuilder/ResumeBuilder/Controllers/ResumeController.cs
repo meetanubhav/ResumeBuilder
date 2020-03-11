5ea87ff3180ec548b8731f3b33eb67728de8fa4a
rishabdaga <41539806+rishabdaga@users.noreply.github.com>
2020-03-11 11:49:44 +0530

    cache changes

--- ResumeBuilder/ResumeBuilder/Controllers/ResumeController.cs
+++ ResumeBuilder/ResumeBuilder/Controllers/ResumeController.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+<<<<<<< Updated upstream
 using System.Security.Claims;
 using System.Security.Authentication;
+=======
+using System.Web.Security;
+using System.Data.Entity;
+>>>>>>> Stashed changes
 
 using System.Data.Entity;
 namespace ResumeBuilder.Controllers
@@ -22,8 +27,11 @@ namespace ResumeBuilder.Controllers
         [HttpPost]
         public ActionResult Login(User user)
         {
+            
             if (ModelState.IsValid)
             {
+
+              
                 var getUserId = db.Users.Where(x => x.Username == user.Username);
                 if (getUserId.Where(x => x.Password == user.Password).Any())
                 { }
@@ -33,6 +41,7 @@ namespace ResumeBuilder.Controllers
                 string message = string.Empty;
                 switch (userId.Value)
                 {
+<<<<<<< Updated upstream
                     case -1:
                         message = "Username and/or password is incorrect.";
                         break;
@@ -42,6 +51,14 @@ namespace ResumeBuilder.Controllers
                     default:
                         FormsAuthentication.SetAuthCookie(user.Username, user.RememberMe);
                         return RedirectToAction("Profile");
+=======
+                    FormsAuthentication.SetAuthCookie(user.Username, false);
+                    var authTicket = new FormsAuthenticationTicket(1, user.Username, DateTime.Now, DateTime.Now.AddMinutes(10), false, "Users");
+                    string encryptedTicket = FormsAuthentication.Encrypt(authTicket);
+                    var authCookie = new HttpCookie(FormsAuthentication.FormsCookieName, encryptedTicket);
+                    HttpContext.Response.Cookies.Add(authCookie);
+                    return RedirectToAction("Dashboard");
+>>>>>>> Stashed changes
                 }
 
                 ViewBag.Message = message;
@@ -49,13 +66,23 @@ namespace ResumeBuilder.Controllers
             }
         }
 
+<<<<<<< Updated upstream
         [Authorize]
+=======
+        [Authorize(Roles = "Users")]
+>>>>>>> Stashed changes
         public ActionResult Dashboard()
         {
 >>>>>>> Stashed changes
             return View();
         }
+<<<<<<< Updated upstream
         public ActionResult Dashboard()
+=======
+
+        [Authorize(Roles = "Users")]
+        public ActionResult Edit()
+>>>>>>> Stashed changes
         {
             return View();
         }
--- ResumeBuilder/ResumeBuilder/Global.asax.cs
+++ ResumeBuilder/ResumeBuilder/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace ResumeBuilder
 {
@@ -13,6 +14,20 @@ namespace ResumeBuilder
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            var authCookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                if (authTicket != null && !authTicket.Expired)
+                {
+                    var roles = authTicket.UserData.Split(',');
+                    HttpContext.Current.User = new System.Security.Principal.GenericPrincipal(new FormsIdentity(authTicket), roles);
+                }
+            }
         }
     }
 }
