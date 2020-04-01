dc7d2694225dbcb8b5d834227233daa257f9d766
rishabdaga <41539806+rishabdaga@users.noreply.github.com>
2020-04-01 12:45:57 +0530

    addedn hashing and data annotations for password

--- ResumeBuilder/ResumeBuilder/Models/ViewModel/LoginVM.cs
+++ ResumeBuilder/ResumeBuilder/Models/ViewModel/LoginVM.cs
@@ -13,6 +13,7 @@ namespace ResumeBuilder.Models.ViewModel
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [RegularExpression("^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$", ErrorMessage = "Password must contain at least one letter, at least one number, and be longer than six charaters.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
--- ResumeBuilder/ResumeBuilder/Models/ViewModel/RegisterVM.cs
+++ ResumeBuilder/ResumeBuilder/Models/ViewModel/RegisterVM.cs
@@ -14,12 +14,14 @@ namespace ResumeBuilder.Models.ViewModel
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [RegularExpression("^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$", ErrorMessage = "Password must contain at least one letter, at least one number, and be longer than six charaters.")]
         [Display(Name = "Password")]
         [DataType(DataType.Password)]
         public string RegisterPassword { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [RegularExpression("^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{6,}$", ErrorMessage = "Password must contain at least one letter, at least one number, and be longer than six charaters.")]
         [DataType(DataType.Password)]
         [Compare("RegisterPassword")]
         [Display(Name = "Confirm Password")]
